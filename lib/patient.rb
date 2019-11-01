6ef922526b8e2a4b6439fb19841ec178f62126f8
favorable-device-5158 <blee2125@gmail.com>
2019-11-01 15:27:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,8 +11,16 @@ class Patient
     @@all
   end
 
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
 
-
+  def doctor
+    appointments.map(&:doctor)
+  end
 
 end
